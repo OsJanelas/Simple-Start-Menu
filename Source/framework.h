e9f9b0348f3a050567374e306bbcdc49c6b23a66
OsJanelas <osjanelas@gmail.com>
2026-02-17 13:14:21 -0300

    Update framework.h

--- Source/framework.h
+++ Source/framework.h
@@ -1,10 +1,11 @@
 #pragma once
 
 #include "targetver.h"
-#define WIN32_LEAN_AND_MEAN             // Excluir itens raramente utilizados dos cabeçalhos do Windows
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <stdlib.h>
 #include <malloc.h>
 #include <memory.h>
 #include <tchar.h>
+
 
